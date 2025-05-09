1e4440ac4d52ed9ca48703e84b30a6317860eb14
EC2 Default User <ec2-user@ip-10-0-0-141.ap-northeast-2.compute.internal>
2025-05-09 01:12:49 +0000

    a

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/product", func(c *gin.Context) {
+	c.String(http.StatusOK, "hello, product v30")
+	})
+
+	r.GET("/health", func(c *gin.Context) {
+		c.String(http.StatusOK, "ok")
+	})
+
+	r.Run()
+}
+
