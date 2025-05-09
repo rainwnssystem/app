dbb9025003be616c6cd90ca6c134630506c8df37
EC2 Default User <ec2-user@ip-10-0-0-141.ap-northeast-2.compute.internal>
2025-05-09 01:35:37 +0000

    v4

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/product", func(c *gin.Context) {
-	c.String(http.StatusOK, "hello, product v3")
+	c.String(http.StatusOK, "hello, product v4")
 	})
 
 	r.GET("/health", func(c *gin.Context) {
